dff94315bb5a258d04171fd43af7861fbc28ab58
Dane H <vincentiodane@gmail.com>
2023-11-09 15:11:38 -0700

    load

--- pixelEditorModel.cpp
+++ pixelEditorModel.cpp
@@ -29,12 +29,12 @@ void pixelEditorModel::undo()
 
 void pixelEditorModel::addToRedo()
 {
-
+    //todo
 }
 
 void pixelEditorModel::addToUndo()
 {
-
+    //todo
 }
 
 void pixelEditorModel::changePixel()
@@ -44,28 +44,28 @@ void pixelEditorModel::changePixel()
 
 void pixelEditorModel::selectColor(QColor color)
 {
-    //TODO
+    //emit some signal
 }
 
 void pixelEditorModel::createJSON()
 {
-    spriteJSON.insert("height", height);
-    spriteJSON.insert("width", width);
-    spriteJSON.insert("numberOfFrames", numberOfSprites);
+    spriteJSON.insert("height", sprites[0].height);
+    spriteJSON.insert("width", sprites[0].width);
+    spriteJSON.insert("numberOfFrames", (int)sprites.size());
 
-    QJsonObject frames;
-    for(int spriteIndex = 0; spriteIndex < numberOfSprites; spriteIndex++)
+    QJsonObject frame;
+    for(unsigned int spriteIndex = 0; spriteIndex < sprites.size(); spriteIndex++)
     {
         QJsonArray rows;
         // loops through row of pixel
-        for(int rowIndex = 0; rowIndex < height; rowIndex++)
+        for(int rowIndex = 0; rowIndex < sprites[spriteIndex].height; rowIndex++)
         {
             QJsonArray pixels;
             // loops through each pixel in said row
-            for(int pixelIndex = 0; pixelIndex < width; pixelIndex++)
+            for(int pixelIndex = 0; pixelIndex < sprites[spriteIndex].width; pixelIndex++)
             {
                 QJsonArray colors;
-                QColor currentPixelColor = grid[spriteIndex].pixelColor(pixelIndex, rowIndex);
+                QColor currentPixelColor = sprites[spriteIndex].getColor(pixelIndex, rowIndex);
                 colors.push_back(currentPixelColor.red());
                 colors.push_back(currentPixelColor.green());
                 colors.push_back(currentPixelColor.blue());
@@ -76,9 +76,9 @@ void pixelEditorModel::createJSON()
             rows.push_back(pixels);
         }
         // Each frames holds an array of rows that describe a sprite
-        frames.insert("frame" + QString::number(spriteIndex), rows);
+        frame.insert("frame" + QString::number(spriteIndex), rows);
     }
-    spriteJSON.insert("frames", frames);
+    spriteJSON.insert("frame", frame);
 }
 
 void pixelEditorModel::save(QString filename)
@@ -92,9 +92,59 @@ void pixelEditorModel::save(QString filename)
     saveFile.write(saveDoc.toJson());
 }
 
-void pixelEditorModel::load(QString filepath)
+void pixelEditorModel::load(QString filename)
 {
-    //TODO
+    try
+    {
+        QFile loadFile(filename);
+        if (!loadFile.open(QIODevice::ReadOnly))
+            qWarning("unable to load file");
+
+        QByteArray saveData = loadFile.readAll();
+        QJsonDocument loadDocument(QJsonDocument::fromJson(saveData));
+        QJsonObject savedData = loadDocument.object();
+
+        QJsonValue height = savedData.value("height");
+        int spriteHeight = height.toInt();
+        QJsonValue width = savedData.value("width");
+        int spriteWidth = width.toInt();
+        QJsonValue numberOfFrames = savedData.value("numberOfFrames");
+        int numberOfSprites = numberOfFrames.toInt();
+
+        QJsonObject frames = savedData.value("frames").toObject();
+        sprites.clear();
+
+        for(int imageIndex = 0; imageIndex < numberOfSprites; imageIndex++)
+        {
+            QImage img = QImage(spriteWidth, spriteHeight, QImage::Format_ARGB32);
+            sprites.push_back(img);
+
+            QString rowName = "frame" + QString::number(imageIndex);
+            QJsonArray rows = frames.value(rowName).toArray();
+
+            // Accesses each row of pixels in an image
+            for(int rowIndex = 0; rowIndex < spriteHeight; rowIndex++)
+            {
+                QJsonArray pixels = rows.at(rowIndex).toArray();
+                // Accesses each pixel in a particular row of an image
+                for(int pixelIndex = 0; pixelIndex < spriteWidth; pixelIndex++)
+                {
+                    QJsonArray colors = pixels.at(pixelIndex).toArray();
+                    int red = colors.at(0).toInt();
+                    int green = colors.at(1).toInt();
+                    int blue = colors.at(2).toInt();
+                    int alpha = colors.at(3).toInt();
+                    QColor currentPixelColor(red, green, blue, alpha);
+                    // Draws each pixel in the loaded image with the read color data
+                    sprites[imageIndex].setColor(pixelIndex, rowIndex, currentPixelColor);
+                }
+            }
+        }
+    }
+    catch (...)
+    {
+
+    }
 }
 
 void pixelEditorModel::panUp()
